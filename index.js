a2fdeeb6848337c6bec778ba809ec5b7fbd92049
ebssgd <iamebssgd@yahoo.com>
2023-04-01 19:45:20 -0500

    Drew aliens, added movement

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ let ship = {
 }
 
 let shipImg;
+let shipVelocityX = tileSize; //Moving speed of ship, which is 1 tile size at a time
+
+//Aliens
+let alienArray = [];
+let alienWidth = tileSize * 2;
+let alienHeight = tileSize;
+let alienX = tileSize;
+let alienY = tileSize;
+let alienImg;
+let alienRows = 2;
+let alienColumns = 3;
+let alienCount = 0;
+alienVelocityX = 1;
 
 window.onload = function(){
     board = document.getElementById("board");
@@ -40,4 +53,67 @@ window.onload = function(){
     shipImg.onload = function(){
         context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     }
+
+    alienImg = new Image();
+    alienImg.src = "./images/alien.png";
+    createAliens();
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveShip);
+}
+
+function update(){
+    requestAnimationFrame(update);
+
+    context.clearRect(0, 0, board.width, board.height);
+
+    //Redrawing ship with each movement
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+
+    //Drawing aliens
+    for(let i = 0; i < alienArray.length; i++){
+        let alien = alienArray[i];
+        if(alien.alive){
+            alien.x += alienVelocityX;
+
+            //If alien touches borders
+            if(alien.x + alien.width >= board.width || alien.x <= 0){
+                alienVelocityX *= -1;
+                alien.x += alienVelocityX * 2;
+
+                //Move aliens closer to ship when they hit a border
+                for(let j = 0; j < alienArray.length; j++){
+                    alienArray[j].y += alienHeight;
+                }
+            }
+            context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height);
+        }
+    }
+}
+
+function moveShip(e){
+    if(e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0){
+        ship.x -= shipVelocityX;
+    }else if(e.code =="ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width){
+        ship.x += shipVelocityX;
+    }
+}
+
+function createAliens(){
+    for(let c = 0; c < alienColumns; c++){
+        for(let r = 0; r < alienRows; r++){
+            let alien = {
+                img: alienImg,
+                x: alienX + c * alienWidth,
+                y: alienY + r * alienHeight,
+                width: alienWidth,
+                height: alienHeight,
+                alive: true
+            }
+
+            alienArray.push(alien);
+        }
+    }
+
+    alienCount = alienArray.length;
 }
